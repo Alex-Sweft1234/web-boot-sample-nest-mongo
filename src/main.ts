febb0d55996b546cbb3a753bea172ed7b70c2647
babaev <babaev@atoms.ru>
2022-11-02 21:47:11 +0300

    redis adapter

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new UnauthorizedExceptionFilter());
