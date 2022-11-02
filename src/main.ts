e20a685f6c38d432e691d1df7745db69afca772f
babaev <babaev@atoms.ru>
2022-11-02 20:54:52 +0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { ValidationPipe } from '@nestjs/common';
 declare const module: any;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new UnauthorizedExceptionFilter());
